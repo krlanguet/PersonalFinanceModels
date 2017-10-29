a7c3276cefdb019b18b3c5f9a07ee21f7dc6a802
Keith R Languet <krlanguet@gmail.com>
2017-10-29 18:37:58 -0500

    Added main file

---
+++ main.py
@@ -0,0 +1,17 @@
+#import math as m
+
+def total_investment_value(rate, principle, time, salary):
+    interest = 0
+    for i in range(time):
+        interest =  principle*rate
+        principle += interest + salary
+    return principle
+
+
+rate = 0.04 # yearly return rate
+salary = 68000 # what one makes each year
+principle = 68000 # initial investment
+time = 13 # years
+
+new_amount = total_investment_value(rate, principle, time, salary)
+print(new_amount)
